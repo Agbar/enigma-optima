d60d6b081587721ba3b3d7dada664107f0727b91
Agbar <Agbar@localhost>
2013-09-03 17:58:07 +0200

    Wrap __builtin_shuffle() into PermuteV16qi().

--- x86/cipher_ssse3.h
+++ x86/cipher_ssse3.h
@@ -28,33 +28,38 @@ extern enigma_cipher_function_t enigma_cipher_decoder_lookup_ssse3;
 //v16qi enigma_cipher_decode_ssse3(int text_bite, int lookupNumber, const Key* const restrict key);
 
 inline
+v16qi PermuteV16qi(const PermutationMap_t* map, v16qi vec ){
+    return __builtin_shuffle( map->half[0], map->half[1], vec );
+}
+
+inline
 v16qi enigma_cipher_decode_ssse3( int biteNumber, int lookupNumber, const Key* const restrict key )
 {
     v16qi bite = ciphertext.vector16[biteNumber];
 
     // stbrett forward
-    bite = __builtin_shuffle( key->stbrett.half[0], key->stbrett.half[1], bite );
+    bite = PermuteV16qi ( &key->stbrett, bite );
 
     const struct LookupChunk_t* const restrict lookup = &PathLookupSsse3.lookups[lookupNumber];
 
     bite += lookup->rRingOffset;
     bite -= ( bite >= 26 ) & 26;
-    bite = __builtin_shuffle( PathLookupSsse3.r_ring[0].half[0], PathLookupSsse3.r_ring[0].half[1], bite );
+    bite = PermuteV16qi( &PathLookupSsse3.r_ring[0], bite );
     bite -= lookup->rRingOffset;
     bite += ( bite < 0 ) & 26;
 
     // m+l rings and ukw
-    bite = __builtin_shuffle( lookup->mapping.half[0],  lookup->mapping.half[1],  bite );
+    bite = PermuteV16qi( &lookup->mapping,  bite );
 
     // right ring backwards
     bite += lookup->rRingOffset;
     bite -= ( bite >= 26 ) & 26;
-    bite = __builtin_shuffle( PathLookupSsse3.r_ring[1].half[0], PathLookupSsse3.r_ring[1].half[1], bite );
+    bite = PermuteV16qi( &PathLookupSsse3.r_ring[1], bite );
     bite -= lookup->rRingOffset;
     bite += ( bite < 0 ) & 26;
 
     //stbrett backwards
-    bite = __builtin_shuffle( key->stbrett.half[0], key->stbrett.half[1], bite );
+    bite = PermuteV16qi( &key->stbrett, bite );
 
     return bite & PathLookupSsse3.lookups[lookupNumber].mask;
 }
