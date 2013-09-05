37006801d74fe0cc24d18ad932b3d3c07b4a0675
Agbar <devnull@localhost>
2013-09-05 19:41:39 +0200

    Add more vector types. Fix v16qi: now it's based on char (default) not signed char, as it was incompatible with GCC builtins.

--- config/types.h
+++ config/types.h
@@ -9,14 +9,46 @@ typedef uint32_t dict_t;
 // At most one cycle more when load is from memory on AMDs.
 typedef int8_t text_t;
 
+/* vectors
+Naming guide for GCC vector naming scheme. (I have not found it elsewhere).
+Type name is built like: vNNlt.
+NN: number of elements
+l: length of element, coded as:
+               bits type                asm
+    q: quarter   8  char                byte
+    h: half     16  short               word
+    s: single   32  int, float          double word
+    d: double   64  long long, double   quad word
+t: type
+    i: integer (default)
+    f: floating point
+I extend type to allow:
+    s: signed
+    u: unsigned
+as sometimes it is convenient ie. unsigned right shuffles, etc.
+ * * * * * * * * * * */
+/** \brief Vector of 16 unsigned bytes. 128 bit.
+ */
+typedef unsigned char   v16qu   __attribute__ ((vector_size(16)));
 
+/** \brief Vector of 16 bytes. 128 bit, default signedness.
+ */
+typedef char            v16qi   __attribute__ ((vector_size(16)));
 
-// vectors
-typedef unsigned char v16qu __attribute__ ((vector_size(16)));
-typedef   signed char v16qi __attribute__ ((vector_size(16)));
+/** \brief Vector of 8 words. 128 bit, default signedness.
+ */
+typedef signed short    v8hi    __attribute__ ((vector_size(16)));
 
-typedef unsigned char v32qu __attribute__ ((vector_size(32)));
-typedef   signed char v32qi __attribute__ ((vector_size(32)));
+/** \brief Vector of 2 quadwords. 128 bit, defauld signedness.
+ */
+typedef long long       v2di    __attribute__ ((vector_size(16)));
 
+/** \brief Vector of 32 unsigned bytes. 256 bit.
+ */
+typedef unsigned char   v32qu   __attribute__ ((vector_size(32)));
+
+/** \brief vector of 32 bytes. 256 bit, default signedness.
+ */
+typedef char            v32qi   __attribute__ ((vector_size(32)));
 
 #endif // CONFIG_TYPES_H
