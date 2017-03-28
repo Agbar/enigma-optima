ed4469d3194192f8d14cf0e913547126e872fd1f
Agbar <Agbar@users.noreply.github.com>
2017-03-28 23:59:15 +0200

    Move config/array_sizes. to libEnigma

--- config/array_sizes.h
+++
@@ -1,7 +0,0 @@
-#ifndef ARRAY_SIZES_H
-#define ARRAY_SIZES_H
-
-#define LAST_DIMENSION 32
-
-
-#endif // ARRAY_SIZES_H
---
+++ libEnigma/include/config/array_sizes.h
@@ -0,0 +1,7 @@
+#ifndef ARRAY_SIZES_H
+#define ARRAY_SIZES_H
+
+#define LAST_DIMENSION 32
+
+
+#endif // ARRAY_SIZES_H
