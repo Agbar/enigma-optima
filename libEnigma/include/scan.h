4b4d2bc438a1a30ac1da1e1770104df15765f6cc
Agbar <Agbar@users.noreply.github.com>
2017-07-02 16:53:17 +0200

    Use #pragma once in scan.h

--- libEnigma/include/scan.h
+++ libEnigma/include/scan.h
@@ -1,11 +1,8 @@
-#ifndef SCAN_H
-#define SCAN_H
+#pragma once
 
 unsigned int scan_uint32(char *s, unsigned int lb, unsigned int hb);
 int scan_int32(char *s, int lb, int hb);
 int scan_posint(char *s);
-
-#endif
 
 
 /*
@@ -13,5 +10,5 @@ int scan_posint(char *s);
  * of the General Public License (GPL), version 2. See doc/COPYING for details.
  *
  * Copyright (C) 2005 Stefan Krah
- * 
+ *
  */
