81e41ac5057a02c4790311d7c110860ddad0f3fd
Agbar <Agbar@users.noreply.github.com>
2017-08-19 14:33:06 +0200

    Use pragma once in result.h

--- libEnigma/include/result.h
+++ libEnigma/include/result.h
@@ -1,5 +1,4 @@
-#ifndef RESULT_H
-#define RESULT_H
+#pragma once
 
 #include <stdio.h>
 #include "key.h"
@@ -7,9 +6,6 @@
 FILE *open_outfile(char *s);
 void print_plaintext(FILE *file, const Key* const key, int len);
 void print_key(FILE *file, const Key *key);
-
-#endif
-
 
 /*
  * This file is part of enigma-suite-0.76, which is distributed under the terms
