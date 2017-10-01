8f21e41e669b94bf99e476381b5dc767fac75356
Agbar <Agbar@users.noreply.github.com>
2017-10-01 10:22:23 +0200

    Use prepare_decoder_lookup like in develop branch

--- libEnigma/src/hillclimb2.c
+++ libEnigma/src/hillclimb2.c
@@ -71,6 +71,10 @@ void hillclimb2( const Key *from, const Key *to, const Key *ckey_res, const Key 
   double bestic, ic;
   int firstloop = 1;
 
+  enigma_prepare_decoder_lookup_function_pt prepare_decoder_lookup;
+
+  enigma_cipher_init( enigma_cpu_flags, from->model, &prepare_decoder_lookup );
+
   SetupRandomGenerator();
 
   lastsave = time(NULL);
@@ -162,17 +166,7 @@ void hillclimb2( const Key *from, const Key *to, const Key *ckey_res, const Key 
                ckey.count = 0;
 
                /* initialize path_lookup */
-               switch (m) {
-                 case H: case M3:
-                   init_path_lookup_H_M3(&ckey, len);
-                   break;
-                 case M4:
-                   init_path_lookup_ALL(&ckey, len);
-                   break;
-                 default:
-                   break;
-               }
-             
+               prepare_decoder_lookup( &ckey, len );
 
                /* ic score */
                bestic = icscore(ckey.stbrett, len);
