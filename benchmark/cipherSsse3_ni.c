07fc3dfc16f4706a9f9acdb433bca11c446baeff
Agbar <Agbar@users.noreply.github.com>
2017-11-08 08:26:56 +0100

    Add separate benchmarks for SSSE3 decoding and triscore speeds

---
+++ benchmark/cipherSsse3_ni.c
@@ -0,0 +1,21 @@
+#pragma GCC target ("ssse3")
+
+#ifndef __SSSE3__
+# error SSSE3 not defined
+#endif
+
+
+#include <immintrin.h>
+
+#include "cipherSsse3_ni.h"
+#include "x86/cipherSsse3_inlines.h"
+
+__attribute__ ((flatten))
+void DecodeMessageSsse3( const Key* const restrict key, int len ) {
+    DecodeScoredMessagePartSsse3( key, len, &decodedMsgPartSsse3 );
+}
+
+__attribute__ ((flatten))
+int TriscoreSse3( scoreLength_t len ) {
+    return ComputeTriscoreFromDecodedMsgSse2( &decodedMsgPartSsse3, len );
+}
---
+++ benchmark/computeTriscoreSsse3.tests.cpp
@@ -0,0 +1,96 @@
+#include <benchmark/benchmark.h>
+
+extern "C" {
+#include "charmap.h"
+#include "ciphertext.h"
+#include "dict.h"
+#include "input.h"
+#include "stecker.h"
+#include "x86/cipherSsse3.h"
+#include "cipherSsse3_ni.h"
+}
+
+struct compute_triscore
+    : public benchmark::Fixture {
+    size_t len = 0;
+    Key key {};
+
+    void SetUp(benchmark::State& st) override {
+        init_charmap();
+
+        load_tridict( "00trigr.AVv1" );
+
+        // Score: 46438
+        // UKW: B       // ukwnum
+        // W/0: 532     // slot
+        // Stecker: AXBZCNDPEOJMLSQV
+        // Rings: AAA
+        // Message key: AVA
+
+        const unsigned char ct[] = "pbnxasmdaxnooyhrczgvvzcbigibgwhm"
+                                   "xkrrvqcfjczptunswaddstigqqcsagpk"
+                                   "rxxlomgfxaphhmrfsdkytmypmvrohasq"
+                                   "yrwfwvavgccudbibxxdyzsacjsyotmwu"
+                                   "cnwomhhjpywdcclupgswclmbczssyxpg"
+                                   "mgmqxaufulnozeqenheizzaklc";
+
+        for( size_t i = 0 ; i < sizeof ct; ++i ) {
+            ciphertext.plain[i] = code[ct[i]];
+        }
+
+
+        key.slot = { /*.g:*/ 0, /*.l:*/ 5, /*.m:*/3, /*.r:*/ 2 };
+        key.ring = { /*.g:*/ 0, /*.l:*/ 0, /*.m:*/0, /*.r:*/ 0 }; // AAA
+        key.mesg = { /*.g:*/ 0, /*.l:*/ 0, /*.m:*/'V' - 'A', /*.r:*/ 0 }; // AVA
+        key.ukwnum = 'B' - 'A';
+        key.model = EnigmaModel_M3;
+
+        Fill0To25( key.stbrett.letters );
+        Fill0To25( key.sf );
+        char stecker[] = "AXBZCNDPEOJMLSQV";
+        set_stecker( &key, stecker );
+        get_stecker( &key );
+
+        len = sizeof ct - 1;
+
+    }
+};
+
+BENCHMARK_DEFINE_F( compute_triscore, sse2 ) ( benchmark::State& state ){
+    if( !__builtin_cpu_supports("sse2") ) {
+        state.SkipWithError("SSSE3 not supported");
+        return;
+    }
+    enigma_cipher_decoder_lookup_ssse3.prepare_decoder_lookup_M_H3( &key, len );
+
+    DecodeMessageSsse3( &key, len );
+
+    int score = 0;
+    while( state.KeepRunning() ) {
+        score = TriscoreSse3( len );
+    }
+
+    if( score != 46438 ) {
+        state.SkipWithError( "Wrong score!" );
+    }
+
+    state.SetBytesProcessed( state.iterations() * len );
+}
+
+BENCHMARK_DEFINE_F( compute_triscore, decode_ssse3 ) ( benchmark::State& state ){
+    if( !__builtin_cpu_supports("ssse3") ) {
+        state.SkipWithError("SSSE3 not supported");
+        return;
+    }
+    enigma_cipher_decoder_lookup_ssse3.prepare_decoder_lookup_M_H3( &key, len );
+
+    while( state.KeepRunning() ) {
+        DecodeMessageSsse3( &key, len );
+    }
+
+    state.SetBytesProcessed( state.iterations() * len );
+}
+
+
+BENCHMARK_REGISTER_F( compute_triscore, decode_ssse3 );
+BENCHMARK_REGISTER_F( compute_triscore, sse2 );
