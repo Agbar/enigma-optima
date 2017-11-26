777afe0282ee15558f1b55d79b23806f37fa584c
Agbar <Agbar@users.noreply.github.com>
2017-11-26 10:59:14 +0100

    Add triscore/avx benchmark

--- benchmark/triscore.tests.cpp
+++ benchmark/triscore.tests.cpp
@@ -8,6 +8,7 @@ extern "C" {
 #include "scoreSimple.h"
 #include "x86/cipherSsse3.h"
 #include "x86/scoreSsse3.h"
+#include "x86/scoreAvx.h"
 #include "x86/cipherAvx2.h"
 #include "x86/scoreAvx2.h"
 }
@@ -86,6 +87,23 @@ BENCHMARK_DEFINE_F( triscore, ssse3 ) ( benchmark::State& state ){
     state.SetBytesProcessed( state.iterations() * len );
 }
 
+BENCHMARK_DEFINE_F( triscore, avx ) ( benchmark::State& state ){
+    if( !__builtin_cpu_supports("avx") ) {
+        state.SkipWithError("AVX not supported");
+        return;
+    }
+    enigma_cipher_decoder_lookup_ssse3.prepare_decoder_lookup_M_H3( &key, len );
+
+    int score = 0;
+    for( auto _ : state ) {
+        score = enigmaScoreAvx.triscore( &key, len );
+    }
+    if( score != expectedScore ) {
+        state.SkipWithError( "Wrong score!" );
+    }
+    state.SetBytesProcessed( state.iterations() * len );
+}
+
 BENCHMARK_DEFINE_F( triscore, avx2 ) ( benchmark::State& state ){
     if( !__builtin_cpu_supports("avx2") ) {
         state.SkipWithError("AVX2 not supported");
@@ -109,4 +127,5 @@ BENCHMARK_REGISTER_F( triscore, simple );
 BENCHMARK_REGISTER_F( triscore, basic_no_interleave );
 BENCHMARK_REGISTER_F( triscore, basic );
 BENCHMARK_REGISTER_F( triscore, ssse3 );
+BENCHMARK_REGISTER_F( triscore, avx );
 BENCHMARK_REGISTER_F( triscore, avx2 );
