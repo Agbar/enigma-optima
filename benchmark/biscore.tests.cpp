d2a08c8f2dc10845e4a2f41b0e636e8572c5fa2c
Agbar <Agbar@users.noreply.github.com>
2017-11-26 11:00:52 +0100

    Add biscore/avx benchmark

--- benchmark/biscore.tests.cpp
+++ benchmark/biscore.tests.cpp
@@ -8,6 +8,7 @@ extern "C" {
 #include "scoreSimple.h"
 #include "x86/cipherSsse3.h"
 #include "x86/scoreSsse3.h"
+#include "x86/scoreAvx.h"
 #include "x86/cipherAvx2.h"
 #include "x86/scoreAvx2.h"
 }
@@ -81,6 +82,23 @@ BENCHMARK_DEFINE_F( biscore, ssse3 ) ( benchmark::State& state ){
     state.SetBytesProcessed( state.iterations() * len );
 }
 
+BENCHMARK_DEFINE_F( biscore, avx ) ( benchmark::State& state ){
+    if( !__builtin_cpu_supports("avx") ) {
+        state.SkipWithError("AVX not supported");
+        return;
+    }
+    enigma_cipher_decoder_lookup_ssse3.prepare_decoder_lookup_M_H3( &key, len );
+
+    int score = 0;
+    for( auto _ : state ) {
+        score = enigmaScoreAvx.biscore( &key, len );
+    }
+    if( score != expectedScore ) {
+        state.SkipWithError( "Wrong score!" );
+    }
+    state.SetBytesProcessed( state.iterations() * len );
+}
+
 BENCHMARK_DEFINE_F( biscore, avx2 ) ( benchmark::State& state ){
     if( !__builtin_cpu_supports("avx2") ) {
         state.SkipWithError("AVX2 not supported");
@@ -102,4 +120,5 @@ BENCHMARK_REGISTER_F( biscore, simple );
 BENCHMARK_REGISTER_F( biscore, basic_no_interleave );
 BENCHMARK_REGISTER_F( biscore, basic );
 BENCHMARK_REGISTER_F( biscore, ssse3 );
+BENCHMARK_REGISTER_F( biscore, avx );
 BENCHMARK_REGISTER_F( biscore, avx2 );
