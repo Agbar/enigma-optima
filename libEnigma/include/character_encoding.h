642a0138c5c8536e263f88d65db61e1f0066a81c
Agbar <Agbar@users.noreply.github.com>
2019-02-16 00:00:32 +0100

    Use STATIC_ASSERT instead of tests to check enigma_character's size

--- libEnigma/include/character_encoding.h
+++ libEnigma/include/character_encoding.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include "global.h"
 #include "config/types.h"
 
 struct enigma_character 
@@ -38,3 +39,10 @@ union v32_echar_delta
     struct enigma_char_delta v_ecd[32];
     v32qi vector;
 };
+
+STATIC_ASSERT( sizeof(struct enigma_character) == 1, "enigma_character must be compact" );
+STATIC_ASSERT( sizeof(union v16_echar) == 16, "vector and array of characters must have the same length");
+STATIC_ASSERT( sizeof(union v32_echar) == 32, "vector and array of characters must have the same length");
+STATIC_ASSERT( sizeof(struct enigma_char_delta) == 1, "enigma_character_delta must be compact" );
+STATIC_ASSERT( sizeof(union v16_echar_delta) == 16, "vector and array of character deltas must have the same length");
+STATIC_ASSERT( sizeof(union v32_echar_delta) == 32, "vector and array of character deltas must have the same length");
