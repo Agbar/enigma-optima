20ad418b97612f6d779679395d6ec1acfe6d5a59
Agbar <Agbar@users.noreply.github.com>
2019-05-09 08:30:09 +0200

    Fix IC score for messages longer than 181 chars

--- libEnigma/src/x86/computeScoreAvx2.c
+++ libEnigma/src/x86/computeScoreAvx2.c
@@ -19,15 +19,21 @@ uint16_t ComputeIcscoreFromDecodedMsgAvx2( union ScoringDecodedMessage* msg, sco
 
     v32qi v = (v32qi) _mm256_load_si256( &u.fv );
 
-    // short result[i] = v0[2*i] * ( v0[2*i] - 1 ) + v0[2*i+1] * ( v0[2*i+1] - 1 );
-    __m256i foo   = _mm256_maddubs_epi16( (__m256i)v, (__m256i)( v + -1 ) );
-    
-    __m256i hexFF00 = _mm256_set1_epi16( 0xFF00 );
-    __m256i bar = _mm256_andnot_si256( hexFF00, foo );
-            foo = _mm256_and_si256( hexFF00, foo );
-    
     __m256i zero  = {};
-    __m256i high_sum =_mm256_sad_epu8 ( foo, zero );
+    v16hu foo;
+    if( len <= 181 ) {
+        // short result[i] = v0[2*i] * ( v0[2*i] - 1 ) + v0[2*i+1] * ( v0[2*i+1] - 1 );
+        foo = (v16hu)_mm256_maddubs_epi16( (__m256i)v, (__m256i)( v + -1 ) );
+    } else {
+        v16hu lo = (v16hu)_mm256_unpacklo_epi8( (__m256i)v, zero );
+        v16hu hi = (v16hu)_mm256_unpackhi_epi8( (__m256i)v, zero );
+        foo = lo * ( lo + -1 ) + hi * (hi + -1);
+    }
+    __m256i hexFF00 = _mm256_set1_epi16( 0xFF00 );
+    __m256i bar = _mm256_andnot_si256( hexFF00, (__m256i)foo );
+            foo = (v16hu)_mm256_and_si256( hexFF00, (__m256i)foo );
+
+    __m256i high_sum =_mm256_sad_epu8 ( (__m256i)foo, zero );
     v16hu high = (v16hu)_mm256_slli_si256( high_sum, 1 ); // * 256
     v16hu low  = (v16hu)_mm256_sad_epu8(  bar, zero );
     v16hu vSum = high + low;
