57acc0008c8706c93b5008fe1a20623ccc2c92d5
Agbar <Agbar@users.noreply.github.com>
2019-07-14 13:38:19 +0200

    Add license info to krah_optimizer.c

--- libEnigma/src/stbrett/krah_optimizer.c
+++ libEnigma/src/stbrett/krah_optimizer.c
@@ -1,3 +1,11 @@
+/*
+ * This file is part of enigma-suite-0.76, which is distributed under the terms
+ * of the General Public License (GPL), version 2. See doc/COPYING for details.
+ *
+ * Copyright (C) 2005 Stefan Krah
+ *
+ */
+
 #include "stbrett/krah_optimizer.h"
 
 #include "key.h"
