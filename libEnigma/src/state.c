6aa03b542ac4b875b5ee5a044bdef851bccb6a13
Agbar <Agbar@users.noreply.github.com>
2019-07-14 21:57:50 +0200

    Improve formatting in state.c

--- libEnigma/src/state.c
+++ libEnigma/src/state.c
@@ -6,21 +6,20 @@
 
 void save_state( State state )
 {
-  FILE *fp;
-
-  if ((fp = fopen("00hc.resume", "w")) == NULL)
-    err_open_fatal("00hc.resume");
-
-  print_state(fp, &state);
-  if (ferror(fp) != 0)
-    err_stream_fatal("00hc.resume");
-
-  fclose(fp);
+    FILE *fp = fopen( "00hc.resume", "w" );
+    if ( fp  == NULL ){
+        err_open_fatal( "00hc.resume" );
+    }
+    print_state( fp, &state );
+    if ( ferror( fp ) != 0 ){
+        err_stream_fatal( "00hc.resume" );
+    }
+    fclose(fp);
 }
 
 NO_RETURN
 void save_state_exit( State state, int retval )
 {
-  save_state( state );
-  exit(retval);
+    save_state( state );
+    exit( retval );
 }
