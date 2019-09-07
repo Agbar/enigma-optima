2bc23831605a4bb36a9f1d246afca3b2de32d535
Agbar <Agbar@users.noreply.github.com>
2019-09-07 13:42:38 +0200

    Implement score_optimizer_loop (Krah version)
    Similar to change made to V3NEA's.
    Minimize a range of x,z variables.

--- libEnigma/src/stbrett/krah_optimizer.c
+++ libEnigma/src/stbrett/krah_optimizer.c
@@ -29,6 +29,9 @@ static void OptimizeIcscore ( const struct echar var[26], struct Key* const ckey
 static void OptimizeBiscore ( const struct echar var[26], struct Key* const ckey, int len, const enigma_score_function_t* const sf );
 static void OptimizeTriscore( const struct echar var[26], struct Key* const ckey, int len, const enigma_score_function_t* const sf );
 
+typedef int (score_f) ( const struct Key* restrict key, scoreLength_t length );
+static uint32_t score_optimizer_loop ( const struct echar var[26], struct Key* const ckey, int len, score_f* score, uint32_t start_score );
+
 
 uint32_t
 stbrett_optimize_krah(
@@ -105,6 +108,173 @@ stbrett_optimize_krah(
     return bestscore;
 }
 
+NO_INLINE
+static uint32_t score_optimizer_loop (
+      const struct echar var[26]
+    , struct Key* const ckey
+    , int len
+    , score_f* score
+    , uint32_t start_score ) {
+    uint32_t best_score  = start_score;
+    enum Action_t action = NONE;
+    for( int i = 0; i < 26; i++ ) {
+        for( int k = i + 1; k < 26; k++ ) {
+            if(  ( echar_eq( var[i], ckey->stbrett.letters[ echar_0_based_index( var[i] ) ] )
+                && echar_eq( var[k], ckey->stbrett.letters[ echar_0_based_index( var[k] ) ] )
+                 )
+            ||
+                 ( echar_eq( var[i], ckey->stbrett.letters[ echar_0_based_index( var[k] ) ] )
+                && echar_eq( var[k], ckey->stbrett.letters[ echar_0_based_index( var[i] ) ] )
+                 )
+            ){
+                SwapStbrett( ckey, var[i], var[k] );
+                uint32_t s = score( ckey, len );
+                if( s  > best_score ) {
+                    best_score = s;
+                    continue;
+                }
+                SwapStbrett( ckey, var[i], var[k] );
+            }
+            else if(    echar_eq( var[i], ckey->stbrett.letters[ echar_0_based_index( var[i] ) ] )
+                    && echar_neq( var[k], ckey->stbrett.letters[ echar_0_based_index( var[k] ) ] )
+            ){
+                action = NONE;
+                struct echar z = ckey->stbrett.letters[ echar_0_based_index( var[k] ) ];
+                SwapStbrett( ckey, var[k], z );
+
+                SwapStbrett( ckey, var[i], var[k] );
+                uint32_t s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = KZ_IK;
+                }
+                SwapStbrett( ckey, var[i], var[k] );
+
+                SwapStbrett( ckey, var[i], z );
+                s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = KZ_IZ;
+                }
+                SwapStbrett( ckey, var[i], z );
+
+                switch( action ) {
+                case KZ_IK:
+                    SwapStbrett( ckey, var[i], var[k] );
+                    break;
+                case KZ_IZ:
+                    SwapStbrett( ckey, var[i], z );
+                    break;
+                case NONE:
+                    SwapStbrett( ckey, var[k], z );
+                    break;
+                default:
+                    break;
+                }
+            }
+            else if(    echar_eq( var[k], ckey->stbrett.letters[ echar_0_based_index( var[k] ) ] )
+                    && echar_neq( var[i], ckey->stbrett.letters[ echar_0_based_index( var[i] ) ] )
+            ){
+                action = NONE;
+                struct echar x = ckey->stbrett.letters[ echar_0_based_index( var[i] ) ];
+                SwapStbrett( ckey, var[i], x );
+
+                SwapStbrett( ckey, var[k], var[i] );
+                uint32_t s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = IX_KI;
+                }
+                SwapStbrett( ckey, var[k], var[i] );
+
+                SwapStbrett( ckey, var[k], x );
+                s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = IX_KX;
+                }
+                SwapStbrett( ckey, var[k], x );
+
+                switch( action ) {
+                case IX_KI:
+                    SwapStbrett( ckey, var[k], var[i] );
+                    break;
+                case IX_KX:
+                    SwapStbrett( ckey, var[k], x );
+                    break;
+                case NONE:
+                    SwapStbrett( ckey, var[i], x );
+                    break;
+                default:
+                    break;
+                }
+            }
+            else if(   echar_neq( var[i], ckey->stbrett.letters[ echar_0_based_index( var[i] ) ] )
+                    && echar_neq( var[k], ckey->stbrett.letters[ echar_0_based_index( var[k] ) ] )
+            ){
+                action = NONE;
+                struct echar x = ckey->stbrett.letters[ echar_0_based_index( var[i] ) ];
+                struct echar z = ckey->stbrett.letters[ echar_0_based_index( var[k] ) ];
+                SwapStbrett( ckey, var[i], x );
+                SwapStbrett( ckey, var[k], z );
+
+                SwapStbrett( ckey, var[i], var[k] );
+                uint32_t s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = IXKZ_IK;
+                }
+                SwapStbrett( ckey, x, z );
+                s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = IXKZ_IKXZ;
+                }
+                SwapStbrett( ckey, x, z );
+                SwapStbrett( ckey, var[i], var[k] );
+
+                SwapStbrett( ckey, var[i], z );
+                s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = IXKZ_IZ;
+                }
+                SwapStbrett( ckey, x, var[k] );
+                s = score( ckey, len );
+                if( s > best_score ) {
+                    best_score = s;
+                    action = IXKZ_IZXK;
+                }
+                SwapStbrett( ckey, x, var[k] );
+                SwapStbrett( ckey, var[i], z );
+
+                switch( action ) {
+                case IXKZ_IK:
+                    SwapStbrett( ckey, var[i], var[k] );
+                    break;
+                case IXKZ_IZ:
+                    SwapStbrett( ckey, var[i], z );
+                    break;
+                case IXKZ_IKXZ:
+                    SwapStbrett( ckey, var[i], var[k] );
+                    SwapStbrett( ckey, x, z );
+                    break;
+                case IXKZ_IZXK:
+                    SwapStbrett( ckey, var[i], z );
+                    SwapStbrett( ckey, x, var[k] );
+                    break;
+                case NONE:
+                    SwapStbrett( ckey, var[i], x );
+                    SwapStbrett( ckey, var[k], z );
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+    return best_score;
+}
 
 void OptimizeIcscore(
     const struct echar var[26],
