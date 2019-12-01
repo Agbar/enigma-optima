3e69e3f0b1292081fbc8a72861808327b7a82054
Agbar <Agbar@users.noreply.github.com>
2019-12-01 17:03:34 +0100

    More consistent naming with 'dict_loader'

--- libEnigma/src/dict_load.c
+++ libEnigma/src/dict_load.c
@@ -7,49 +7,49 @@
 #include "echar/echar.h"
 #include "error.h"
 
-struct dict_load_strategy;
+struct dict_loader;
 
-struct dicload_vt {
-    bool (*read_line)( struct dict_load_strategy* self, FILE* in_file );
-    void (*store_dict_value)( struct dict_load_strategy* self, const char* filename );
+struct dict_loader_vt {
+    bool (*read_line)( struct dict_loader* self, FILE* in_file );
+    void (*store_dict_value)( struct dict_loader* self, const char* filename );
 };
 
-struct dict_load_strategy {
-    struct dicload_vt* vt;
+struct dict_loader {
+    struct dict_loader_vt* vt;
     char chars[4];
     int log;
 };
 
-static void load_anydict( const char *filename, struct dict_load_strategy* impl );
+static void load_anydict( const char *filename, struct dict_loader* impl );
 
-static bool tri_read_line( struct dict_load_strategy* self, FILE* in_file );
-static void tri_store_dict_value( struct dict_load_strategy* self, const char* filename );
+static bool tri_read_line( struct dict_loader* self, FILE* in_file );
+static void tri_store_dict_value( struct dict_loader* self, const char* filename );
 
-static bool bi_read_line( struct dict_load_strategy* self, FILE* in_file );
-static void bi_store_dict_value( struct dict_load_strategy* self, const char* filename );
+static bool bi_read_line( struct dict_loader* self, FILE* in_file );
+static void bi_store_dict_value( struct dict_loader* self, const char* filename );
 
-static bool uni_read_line( struct dict_load_strategy* self, FILE* in_file );
-static void uni_store_dict_value( struct dict_load_strategy* self, const char* filename );
+static bool uni_read_line( struct dict_loader* self, FILE* in_file );
+static void uni_store_dict_value( struct dict_loader* self, const char* filename );
 
-struct dicload_vt
+struct dict_loader_vt
     tri_load_cls = { tri_read_line, tri_store_dict_value },
      bi_load_cls = {  bi_read_line,  bi_store_dict_value },
     uni_load_cls = { uni_read_line, uni_store_dict_value };
 
 
-bool tri_read_line( struct dict_load_strategy* self, FILE* in_file ) {
+bool tri_read_line( struct dict_loader* self, FILE* in_file ) {
     return fscanf( in_file, "%3s%d", self->chars, &self->log ) != EOF;
 }
 
-bool bi_read_line( struct dict_load_strategy* self, FILE* in_file ) {
+bool bi_read_line( struct dict_loader* self, FILE* in_file ) {
     return fscanf( in_file, "%2s%d", self->chars, &self->log ) != EOF;
 }
 
-bool uni_read_line( struct dict_load_strategy* self, FILE* in_file ) {
+bool uni_read_line( struct dict_loader* self, FILE* in_file ) {
     return fscanf( in_file, "%1s%d", self->chars, &self->log ) != EOF;
 }
 
-void tri_store_dict_value( struct dict_load_strategy* self, const char* filename ) {
+void tri_store_dict_value( struct dict_loader* self, const char* filename ) {
     if ( !echar_can_make_from_ascii( self->chars[0] )
       || !echar_can_make_from_ascii( self->chars[1] )
       || !echar_can_make_from_ascii( self->chars[2] ))
@@ -65,7 +65,7 @@ void tri_store_dict_value( struct dict_load_strategy* self, const char* filename
            [echar_0_based_index( e2 )] = self->log;
 }
 
-void bi_store_dict_value( struct dict_load_strategy* self, const char* filename ) {
+void bi_store_dict_value( struct dict_loader* self, const char* filename ) {
     if ( !echar_can_make_from_ascii( self->chars[0] )
       || !echar_can_make_from_ascii( self->chars[1] ))
     {
@@ -78,7 +78,7 @@ void bi_store_dict_value( struct dict_load_strategy* self, const char* filename 
           [echar_0_based_index( e1 )] = self->log;
 }
 
-void uni_store_dict_value( struct dict_load_strategy* self, const char* filename ) {
+void uni_store_dict_value( struct dict_loader* self, const char* filename ) {
     if ( !echar_can_make_from_ascii( self->chars[0] ))
     {
         err_illegal_char_fatal( filename );
@@ -87,7 +87,7 @@ void uni_store_dict_value( struct dict_load_strategy* self, const char* filename
     unidict[echar_0_based_index( e )] = self->log;
 }
 
-void load_anydict( const char *filename, struct dict_load_strategy* impl ) {
+void load_anydict( const char *filename, struct dict_loader* impl ) {
     FILE* fp = fopen( filename, "r" );
     if ( !fp ) err_open_fatal( filename );
 
@@ -99,14 +99,14 @@ void load_anydict( const char *filename, struct dict_load_strategy* impl ) {
 
 int load_tridict(const char *filename)
 {
-    struct dict_load_strategy tri = { .vt = &tri_load_cls };
+    struct dict_loader tri = { .vt = &tri_load_cls };
     load_anydict( filename, &tri );
     return 0;
 }
 
 int load_bidict(const char *filename)
 {
-    struct dict_load_strategy bi = { .vt = &bi_load_cls };
+    struct dict_loader bi = { .vt = &bi_load_cls };
     load_anydict( filename, &bi );
     return 0;
 }
@@ -114,7 +114,7 @@ int load_bidict(const char *filename)
 
 int load_unidict(const char *filename)
 {
-    struct dict_load_strategy uni = { .vt = &uni_load_cls };
+    struct dict_loader uni = { .vt = &uni_load_cls };
     load_anydict( filename, &uni );
     return 0;
 }
