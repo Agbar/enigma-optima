b96a3d382b71d3bd01ebfd33dca5393c53f7a2af
Agbar <Agbar@users.noreply.github.com>
2019-12-22 16:32:06 +0100

    Add EnigmaTestData library target

---
+++ tests/data/include/enigma/test_data.h
@@ -0,0 +1,12 @@
+#pragma once
+/*
+ * This file is part of Enigma Optima, which is distributed under the terms
+ * of the General Public License (GPL), version 2. See doc/COPYING for details.
+ *
+ * Copyright (C) 2019 Agbar
+ */
+
+#include <stddef.h>
+
+extern const unsigned int bigraph_cur_size;
+extern const char bigraph_cur[];
