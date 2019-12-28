df60d0687805edb181e892eb9e1ba623aadba459
Agbar <Agbar@users.noreply.github.com>
2019-12-28 17:27:02 +0100

    Change set_ring tests to set_RingState tests

---
+++ tests/unit/input.c/set_RingsState.tests.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "input.h"
+}
+
+
+TEST( set_RingsState_H_M3, loads_example )
+{
+    // invalid
+    RingsState rs {
+        .g = { UINT8_MAX },
+        .l = { UINT8_MAX },
+        .m = { UINT8_MAX },
+        .r = { UINT8_MAX } };
+    char s[] = "ABC";
+
+    int result = set_RingsState( &rs, s, ModelType_t::EnigmaModel_H );
+    ASSERT_TRUE( result );
+
+    ASSERT_EQ( rs.l.delta, 0 );
+    ASSERT_EQ( rs.m.delta, 1 );
+    ASSERT_EQ( rs.r.delta, 2 );
+}
+
+
+TEST( set_RingsState_M4, loads_example )
+{
+    // invalid
+    RingsState rs {
+        .g = { UINT8_MAX },
+        .l = { UINT8_MAX },
+        .m = { UINT8_MAX },
+        .r = { UINT8_MAX } };
+    char s[] = "ABCD";
+
+    int result = set_RingsState( &rs, s, ModelType_t::EnigmaModel_M4 );
+    ASSERT_TRUE( result );
+
+    ASSERT_EQ( rs.g.delta, 0 );
+    ASSERT_EQ( rs.l.delta, 1 );
+    ASSERT_EQ( rs.m.delta, 2 );
+    ASSERT_EQ( rs.r.delta, 3 );
+}
+
+
+TEST( set_RingsState_M4, fails_when_only_3_chars )
+{
+    // invalid
+    RingsState rs {
+        .g = { UINT8_MAX },
+        .l = { UINT8_MAX },
+        .m = { UINT8_MAX },
+        .r = { UINT8_MAX } };
+    char s[] = "ABC";
+
+    int result = set_RingsState( &rs, s, ModelType_t::EnigmaModel_M4 );
+    ASSERT_FALSE( result );
+}
+
+
+TEST( set_RingsState_H_M3, fails_when_4_chars )
+{
+    // invalid
+    RingsState rs {
+        .g = { UINT8_MAX },
+        .l = { UINT8_MAX },
+        .m = { UINT8_MAX },
+        .r = { UINT8_MAX } };
+    char s[] = "ABCD";
+
+    int result = set_RingsState( &rs, s, ModelType_t::EnigmaModel_H );
+    ASSERT_FALSE( result );
+}
+
+
+TEST( set_RingsState, fails_when_not_letter )
+{
+    // invalid
+    RingsState rs {
+        .g = { UINT8_MAX },
+        .l = { UINT8_MAX },
+        .m = { UINT8_MAX },
+        .r = { UINT8_MAX } };
+    char s[] = "AB0";
+
+    int result = set_RingsState( &rs, s, ModelType_t::EnigmaModel_H );
+    ASSERT_FALSE( result );
+}
