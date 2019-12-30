4179eb5d98479dd74a42d44c861b5320a8694358
Agbar <Agbar@users.noreply.github.com>
2019-12-30 22:22:14 +0100

    Add more assertions on optimization results

--- tests/heavy_integration/win_bench_pbnxa.cpp
+++ tests/heavy_integration/win_bench_pbnxa.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdint>
 #include <cstdio>
 #include <exception>
@@ -111,4 +112,30 @@ TEST( win_bench_pbnxa, krah_basic )
         stbrett_optimize_krah
     );
     EXPECT_EQ( gkey.score, 17930 );
+
+    EXPECT_EQ( gkey.ukwnum.type, UkwType::UkwType_B );
+    auto& slot = gkey.slot;
+    EXPECT_EQ( slot.l.type, RingType::RingType_5 );
+    EXPECT_EQ( slot.m.type, RingType::RingType_3 );
+    EXPECT_EQ( slot.r.type, RingType::RingType_2 );
+
+    std::string stecker;
+    std::transform(
+        std::begin( gkey.sf.map ),
+        std::begin( gkey.sf.map ) + gkey.count,
+        std::back_inserter( stecker ),
+        []( const echar& e ){ return echar_to_ALPHA( e ); }
+    );
+    EXPECT_EQ( stecker, "AIBECJDRFYGOHZMUNQPVST" );
+
+    char ring[]{
+        echar_delta_to_ALPHA( gkey.ring.l ),
+        echar_delta_to_ALPHA( gkey.ring.m ),
+        echar_delta_to_ALPHA( gkey.ring.r ), '\0' };
+    EXPECT_STREQ( ring, "AAC" );
+    char mesg[]{
+        echar_delta_to_ALPHA( gkey.mesg.l ),
+        echar_delta_to_ALPHA( gkey.mesg.m ),
+        echar_delta_to_ALPHA( gkey.mesg.r ), '\0' };
+    EXPECT_STREQ( mesg, "HVS" );
 }
