fb96ad3119b884620e9302c312c55fb82ec7d904
Agbar <Agbar@users.noreply.github.com>
2019-12-31 13:12:12 +0100

    Test set_ukw

---
+++ tests/unit/input.c/set_ukw.tests.cpp
@@ -0,0 +1,121 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "input.h"
+}
+
+
+TEST( set_ukw, H_M3_ukw_A ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char A[] = "A";
+    EXPECT_TRUE( set_ukw( &k, A, ModelType_t::EnigmaModel_H ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_A );
+}
+
+
+TEST( set_ukw, H_M3_ukw_a ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char a[] = "a";
+    EXPECT_TRUE( set_ukw( &k, a, ModelType_t::EnigmaModel_H ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_A );
+}
+
+
+TEST( set_ukw, H_M3_ukw_B ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char B[] = "B";
+    EXPECT_TRUE( set_ukw( &k, B, ModelType_t::EnigmaModel_H ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_B );
+}
+
+
+TEST( set_ukw, H_M3_ukw_b ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char b[] = "b";
+    EXPECT_TRUE( set_ukw( &k, b, ModelType_t::EnigmaModel_H ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_B );
+}
+
+
+TEST( set_ukw, H_M3_ukw_C ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char c[] = "C";
+    EXPECT_TRUE( set_ukw( &k, c, ModelType_t::EnigmaModel_H ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_C );
+}
+
+
+TEST( set_ukw, H_M3_ukw_c ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char c[] = "c";
+    EXPECT_TRUE( set_ukw( &k, c, ModelType_t::EnigmaModel_H ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_C );
+}
+
+
+TEST( set_ukw, H_M3_fails_with_other ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char d[] = "d";
+    EXPECT_FALSE( set_ukw( &k, d, ModelType_t::EnigmaModel_H ) );
+}
+
+
+TEST( sek_ukw, M4_ukw_A_fails ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char A[] = "A";
+    EXPECT_FALSE( set_ukw( &k, A, ModelType_t::EnigmaModel_M4 ) );
+    char a[] = "a";
+    EXPECT_FALSE( set_ukw( &k, a, ModelType_t::EnigmaModel_M4 ) );
+}
+
+
+TEST( sek_ukw, M4_ukw_B ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char B[] = "B";
+    EXPECT_TRUE( set_ukw( &k, B, ModelType_t::EnigmaModel_M4 ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_B_Thin );
+}
+
+
+TEST( sek_ukw, M4_ukw_b ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char b[] = "b";
+    EXPECT_TRUE( set_ukw( &k, b, ModelType_t::EnigmaModel_M4 ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_B_Thin );
+}
+
+
+TEST( sek_ukw, M4_ukw_C ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char C[] = "C";
+    EXPECT_TRUE( set_ukw( &k, C, ModelType_t::EnigmaModel_M4 ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_C_Thin );
+}
+
+
+TEST( sek_ukw, M4_ukw_c ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char c[] = "c";
+    EXPECT_TRUE( set_ukw( &k, c, ModelType_t::EnigmaModel_M4 ) );
+    EXPECT_EQ( k.ukwnum.type, UkwType::UkwType_C_Thin );
+}
+
+
+TEST( sek_ukw, M4_ukw_fails_with_other ) {
+    Key k;
+    k.ukwnum.type = (UkwType::ukw_type_enum)-1;
+    char d[] = "d";
+    EXPECT_FALSE( set_ukw( &k, d, ModelType_t::EnigmaModel_M4 ) );
+}
