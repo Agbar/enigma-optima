91887db54a30929361cdc8abc7d33e526841f060
Agbar <Agbar@users.noreply.github.com>
2020-01-02 00:46:53 +0100

    Refactor the test to parametrized test

--- tests/heavy_integration/win_bench_pbnxa.cpp
+++ tests/heavy_integration/win_bench_pbnxa.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <exception>
 #include <gtest/gtest.h>
+#include <tuple>
 
 #include "dicts/dict_builders.hpp"
 #include "dicts/string_dict_loader.hpp"
@@ -52,84 +53,125 @@ void hillclimb_log( const char *s ) {
 
 volatile sig_atomic_t doShutdown;
 
+class Hillclimb_PBNXA
+    : public ::testing::TestWithParam<
+          std::tuple< enigma_prepare_decoder_lookup_function_pt,
+                      enigma_score_function_t* > > {
 
-TEST( win_bench_pbnxa, krah_basic )
-{
-    // command line is:
-    // enigma.exe -M M3 -c -o bench-result.txt -f "B:532:AA:AAA" -t "B:532:AH:ZZZ" 00trigr.cur 00bigr.cur benchmark_cipher
-
-    ASSERT_TRUE( load_tridict( trigraph_cur ) );
-    ASSERT_TRUE( load_bidict( bigraph_cur ) );
-
-    load_message( benchmark_cipher_pbnxa_size, benchmark_cipher_pbnxa );
-
-    Key from;
-    Key to;
-    {
-        auto from_txt = "B:532:AA:AAA";
-        auto to_txt = "B:532:AH:ZZZ";
-        ASSERT_TRUE( set_range( &from, &to, from_txt, to_txt, EnigmaModel_M3 ) );
-    }
-    int len = benchmark_cipher_pbnxa_size - 1;
-    if( len > CT ) len = CT;
-    // make the test deterministic
-    srand( 42 );
-
-    Key ckey = from;
-    Key gkey = from;
-    State state = {
-        from :  &from,
-        to : &to,
-        ckey : &ckey, // not a resume
-        gkey : &gkey, // not a resume
-        sw_mode : SW_ONSTART,
-        pass : 0,
-        firstpass : true,
-        max_score : INT_MAX - 1,
-        ciphertext : ciphertext.plain
-    };
     ScoreOptimizer scOptimizer = {
         optimize_score : stbrett_optimize_krah,
-        prepare_decoder_lookup : enigma_cipher_decoder_lookup.prepare_decoder_lookup_M_H3,
-        score_impl : &enigmaScoreBasic,
+        prepare_decoder_lookup : nullptr,
+        score_impl : nullptr,
     };
-    HillclimbersKnapsack knapsack = {
+    const HillclimbersKnapsack knapsack = {
         optimizer : &scOptimizer,
         on_new_best : []( const struct Key*, int ) {},
         save_state : []( const State*, bool ) { /*NOP*/ },
         log : []( const char[] ) {},
     };
-    hillclimb(
-        &state,
-        /* max_pass */      1,
-        len,
-        &knapsack
-    );
-    EXPECT_EQ( gkey.score, 17930 );
+    Key gkey{};
 
-    EXPECT_EQ( gkey.ukwnum.type, UkwType::UkwType_B );
-    auto& slot = gkey.slot;
-    EXPECT_EQ( slot.l.type, RingType::RingType_5 );
-    EXPECT_EQ( slot.m.type, RingType::RingType_3 );
-    EXPECT_EQ( slot.r.type, RingType::RingType_2 );
+    static Key from;
+    static Key to;
+    static int len;
 
-    std::string stecker;
-    std::transform(
-        std::begin( gkey.sf.map ),
-        std::begin( gkey.sf.map ) + gkey.count,
-        std::back_inserter( stecker ),
-        []( const echar& e ){ return echar_to_ALPHA( e ); }
-    );
-    EXPECT_EQ( stecker, "AIBECJDRFYGOHZMUNQPVST" );
+public:
+    static void SetUpTestCase() {
+        // command line is:
+        // enigma.exe -M M3 -c -o bench-result.txt -f "B:532:AA:AAA" -t
+        // "B:532:AH:ZZZ" 00trigr.cur 00bigr.cur benchmark_cipher
 
-    char ring[]{
-        echar_delta_to_ALPHA( gkey.ring.l ),
-        echar_delta_to_ALPHA( gkey.ring.m ),
-        echar_delta_to_ALPHA( gkey.ring.r ), '\0' };
-    EXPECT_STREQ( ring, "AAC" );
-    char mesg[]{
-        echar_delta_to_ALPHA( gkey.mesg.l ),
-        echar_delta_to_ALPHA( gkey.mesg.m ),
-        echar_delta_to_ALPHA( gkey.mesg.r ), '\0' };
-    EXPECT_STREQ( mesg, "HVS" );
+        ASSERT_TRUE( load_tridict( trigraph_cur ) );
+        ASSERT_TRUE( load_bidict( bigraph_cur ) );
+
+        load_message( benchmark_cipher_pbnxa_size, benchmark_cipher_pbnxa );
+
+        ASSERT_TRUE( set_range( &from,
+                                &to,
+                                "B:532:AA:AAA",
+                                "B:532:AH:ZZZ",
+                                EnigmaModel_M3 ) );
+        len = benchmark_cipher_pbnxa_size - 1;
+        if( len > CT ) len = CT;
+    }
+
+
+protected:
+    void SetUp() override final {
+        std::tie( scOptimizer.prepare_decoder_lookup, scOptimizer.score_impl ) =GetParam();
+    }
+
+
+public:
+    void RunHillclimb() {
+        // make the test deterministic
+        srand( 42 );
+
+        Key ckey = from;
+        gkey = from;
+        State state = {
+            from : &from,
+            to : &to,
+            ckey : &ckey, // not a resume
+            gkey : &gkey, // not a resume
+            sw_mode : SW_ONSTART,
+            pass : 0,
+            firstpass : true,
+            max_score : INT_MAX - 1,
+            ciphertext : ciphertext.plain
+        };
+        hillclimb( &state,
+                   /* max_pass */   1,
+                   len,
+                   &knapsack );
+    }
+
+    void RunAssertions() {
+        EXPECT_EQ( gkey.score, 17930 );
+
+        EXPECT_EQ( gkey.ukwnum.type, UkwType::UkwType_B );
+        auto& slot = gkey.slot;
+        EXPECT_EQ( slot.l.type, RingType::RingType_5 );
+        EXPECT_EQ( slot.m.type, RingType::RingType_3 );
+        EXPECT_EQ( slot.r.type, RingType::RingType_2 );
+
+        std::string stecker;
+        std::transform(
+            std::begin( gkey.sf.map ),
+            std::begin( gkey.sf.map ) + gkey.count,
+            std::back_inserter( stecker ),
+            []( const echar& e ) { return echar_to_ALPHA( e ); } 
+        );
+        EXPECT_EQ( stecker, "AIBECJDRFYGOHZMUNQPVST" );
+
+        char ring[]{
+            echar_delta_to_ALPHA( gkey.ring.l ),
+            echar_delta_to_ALPHA( gkey.ring.m ),
+            echar_delta_to_ALPHA( gkey.ring.r ), '\0' };
+        EXPECT_STREQ( ring, "AAC" );
+        char mesg[]{
+            echar_delta_to_ALPHA( gkey.mesg.l ),
+            echar_delta_to_ALPHA( gkey.mesg.m ),
+            echar_delta_to_ALPHA( gkey.mesg.r ), '\0' };
+        EXPECT_STREQ( mesg, "HVS" );
+    }
+};
+
+
+Key Hillclimb_PBNXA::from;
+Key Hillclimb_PBNXA::to;
+int Hillclimb_PBNXA::len;
+
+
+TEST_P( Hillclimb_PBNXA, KrahOptimizesAsOriginal ) {
+    RunHillclimb();
+    RunAssertions();
 }
+
+
+auto basic = std::make_tuple( enigma_cipher_decoder_lookup.prepare_decoder_lookup_M_H3,
+                              &enigmaScoreBasic );
+
+
+INSTANTIATE_TEST_CASE_P( WinBench, Hillclimb_PBNXA,
+                         ::testing::Values( basic ) );
