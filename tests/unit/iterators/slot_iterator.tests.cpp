00e847008e7c01839cb31e61140a57df985776cf
Agbar <Agbar@users.noreply.github.com>
2020-02-25 18:58:07 +0100

    Test slot_iterator for model M4

--- tests/unit/iterators/slot_iterator.tests.cpp
+++ tests/unit/iterators/slot_iterator.tests.cpp
@@ -41,6 +41,33 @@ static RingTypes operator"" _ringTypes_M3( const char* txt, std::size_t len ) {
 }
 
 
+static RingTypes operator"" _ringTypes_M4( const char* txt, std::size_t len ) {
+    auto alpha_to_RingType = []( const char c ) -> RingType {
+        if( '0' > c || c > '8' ) throw std::logic_error( "Invalid rings literal." );
+        return RingType{static_cast< ring_type_enum >( c - '0' )};
+    };
+    auto alpha_to_GreekRingType = []( const char c ) -> GreekRingType {
+        switch( c ) {
+        case 'B':
+            return GreekRingType{GreekRingType_Beta};
+        case 'G':
+            return GreekRingType{GreekRingType_Gamma};
+        default:
+            throw std::logic_error( "Invalid rings literal." );
+        }
+    };
+
+    if( len == 4 ) {
+        return RingTypes{.g = alpha_to_GreekRingType( txt[ 0 ] ),
+                         .l = alpha_to_RingType( txt[ 1 ] ),
+                         .m = alpha_to_RingType( txt[ 2 ] ),
+                         .r = alpha_to_RingType( txt[ 3 ] )};
+    } else {
+        throw std::logic_error( "Invalid rings literal. Length=" + std::to_string( len ) );
+    }
+}
+
+
 static inline bool operator==( const RingTypes lhs, const RingTypes rhs ) {
     return RingTypes_equ( lhs, rhs );
 }
@@ -267,6 +294,137 @@ TEST_F( slot_iterator_m3, next_after_876_is_123_with_overflow ) {
     slot_iter.next( &slot_iter );
 
     ASSERT_EQ( *slot_iter.state, "123"_ringTypes_M3 );
+    ASSERT_TRUE( slot_iter.bits.og & SlotIterator_bits_overflow );
+    ASSERT_TRUE( SlotIterator_equ( slot_iter, slot_overflow() ) );
+}
+
+
+//============================
+
+
+TEST( SlotIterator_equ_M4, 132_equ_132 ) {
+    const ModelType_t model = EnigmaModel_M4;
+    RingTypes slot = "B128"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+    slot_iter.next( &slot_iter );
+
+    RingTypes expected_132 = "B132"_ringTypes_M4;
+    EXPECT_EQ( *slot_iter.state, expected_132 );
+    ASSERT_TRUE( SlotIterator_equ( slot_iter, init_SlotIterator( &expected_132, model ) ) );
+}
+
+
+class slot_iterator_m4
+    : public ::testing::Test {
+protected:
+    RingTypes slot{
+        g : {GreekRingType_Beta},
+        l : {RingType_1},
+        m : {RingType_2},
+        r : {RingType_3}
+    };
+    const ModelType_t model = EnigmaModel_M4;
+};
+
+
+TEST_F( slot_iterator_m4, init_B111_selects_B123 ) {
+    slot = "B111"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    ASSERT_EQ( *slot_iter.state, "B123"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, count_all ) {
+    uint64_t loop_count = 0;
+    for( SlotIterator slot_iter = init_SlotIterator( &slot, model );
+         !SlotIterator_equ( slot_overflow(), slot_iter );
+         slot_iter.next( &slot_iter ) ) {
+        loop_count++;
+    }
+
+    ASSERT_EQ( loop_count, 2 * 8 * 7 * 6 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B123_is_B124 ) {
+    slot = "B123"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B124"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B125_is_B126 ) {
+    slot = "B125"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B126"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B128_is_B132 ) {
+    slot = "B128"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B132"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B132_is_B134 ) {
+    slot = "B132"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B134"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B218_is_B231 ) {
+    slot = "B218"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B231"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B675_is_B678 ) {
+    slot = "B675"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B678"_ringTypes_M4 );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_B876_is_G123 ) {
+    slot = "B876"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "G123"_ringTypes_M4 );
+    ASSERT_FALSE( slot_iter.bits.og & SlotIterator_bits_overflow );
+}
+
+
+TEST_F( slot_iterator_m4, next_after_G876_is_B123_with_overflow ) {
+    slot = "G876"_ringTypes_M4;
+    SlotIterator slot_iter = init_SlotIterator( &slot, model );
+
+    slot_iter.next( &slot_iter );
+
+    ASSERT_EQ( *slot_iter.state, "B123"_ringTypes_M4 );
     ASSERT_TRUE( slot_iter.bits.og & SlotIterator_bits_overflow );
     ASSERT_TRUE( SlotIterator_equ( slot_iter, slot_overflow() ) );
 }
